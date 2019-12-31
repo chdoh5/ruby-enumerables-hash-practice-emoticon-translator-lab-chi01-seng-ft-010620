2eb75b0c5ab23a4080816cc541ccb65ed0f2c04b
Christine Doherty <christineadoherty@gmail.com>
2019-12-31 19:24:23 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,8 +1,22 @@
-# require modules here
+require "yaml"
 
-def load_library
-  # code goes here
-end
+
+def load_library(file_path)
+  emoticons = YAML.load_file('./lib/emoticons.yml')
+keys = {
+    "get_meaning" => {},
+    "get_emoticon" => {}
+  }
+
+  emoticons.each do |meanings, pictures|
+    pictures.each do |j_version, e_version|
+    keys["get_meaning"][pictures[1]] = meanings
+    keys["get_emoticon"][pictures[0]] = j_version
+    end
+  
+    end
+keys
+  end
 
 def get_japanese_emoticon
   # code goes here
